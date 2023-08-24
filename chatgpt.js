145e7a5f3d11d878b829212706c9877090e20ac4
vickey1213 <vickey.kmr90@gmail.com>
2023-08-24 20:49:25 +0530

    Gift repo

---
+++ chatgpt.js
@@ -0,0 +1,57 @@
+// require("dotenv").config();
+// const express = require("express");
+// const OpenAI = require("openai");
+
+// app.use(express.json());
+// const { Configuration, OpenAIApi } = require("openai");
+// const configuration = new Configuration({
+//   apiKey: process.env.APIKEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// async function runCompletion() {
+//   const completion = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: "How are you today?",
+//   });
+//   console.log(completion.data.choices[0].text);
+// }
+
+// runCompletion();
+
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+// console.log("OPENAI_API_KEY, ", process.env.APIKEY);
+const openai = new OpenAIApi(configuration);
+
+router.post("/gifts", async (req, res, next) => {
+  try {
+    const body = req.body;
+    let prompt = `I want to buy a gift for my ${body.relationship} of age ${body.age}. For the occasion ${body.occasion}. Based on their interests in ${body.interests}, and my budget is ${body.budget}.`;
+
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 100,
+    });
+
+    const generatedText = completion.choices[0].text;
+
+    console.log(generatedText);
+
+    res.redirect("/gifts");
+  } catch (error) {
+    res.redirect("/gifts");
+  }
+});
+
+router.get("/v1", (req, res, next) => {
+  res.send("Hi VIckey");
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const OpenAI = require("openai");
+const app = express();
+const PORT = 3000;
+const jsonData = require("./data/gifts.json");
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+//runCompletion();
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/profile", (req, res) => {
+  const data = {
+    name: "Vickey",
+    lName: "Kumar",
+  };
+  res.render("profile", { data });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/gifts", (req, res) => {
+  res.render("gifts", { gifts: jsonData });
+});
+
+app.use("/chatgpt", require("./chatgpt"));
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT" + PORT);
+});
